8a24d1ef3934ef425e44b14784afa22f4f723200
Ruhan304 <68950742+Ruhan304@users.noreply.github.com>
2021-02-23 15:48:46 +0530

    Add files via upload

---
+++ TowerSiege-1-main/TowerSiege-1-main/polygon.js
@@ -0,0 +1,24 @@
+class Polygon {
+    constructor(x,y){
+      this.image = loadImage("polygon.png");
+      this.trajectory = []
+
+      var options = {
+        friction: 2.5, restitution: 1.1, density: 2
+    }
+    this.width = 100
+    this.height = 100
+    this.body = Bodies.rectangle(x,y,100,100,options)
+    World.add(world,this.body)
+    } 
+  
+    display() {
+imageMode(CENTER)
+image (this.image,this.body.position.x,this.body.position.y,this.width,this.height)
+ /* if(this.body.velocity.x>10&& this.body.position.x>210){
+    var positions = [this.body.position.x, this.body.position.y]
+  this.trajectory.push(positions);
+  }*/
+      
+  }
+  }
